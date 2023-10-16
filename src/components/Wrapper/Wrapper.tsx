ad585d78eac8332a05e1c5ff9d2445404be1c423
Anna Zaryczanska <annazar00@gmail.com>
2023-10-16 21:29:38 +0200

    changing type of children

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import styles from "./Wrapper.module.scss";
 
 interface IWrapper {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 const Wrapper: FC<IWrapper> = ({ children }) => {
